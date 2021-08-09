978e127c2b4e1463a88b62549142acbe56580b29
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-08-09 13:54:28 +0200

    Issue #6: Use model broadcasting events instead custom events

--- src/Models/Observation.php
+++ src/Models/Observation.php
@@ -2,12 +2,14 @@
 
 namespace TromsFylkestrafikk\Meteobridge\Models;
 
+use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use TromsFylkestrafikk\Meteobridge\Events\ObservationCreated;
 
 class Observation extends Model
 {
+    use BroadcastsEvents;
     use HasFactory;
 
     protected $table = 'meteobridge_observations';
@@ -24,5 +26,18 @@ class Observation extends Model
     public function station()
     {
         return $this->belongsTo(Station::class);
+    }
+
+    /**
+     * Get the channel the model should broadcast on.
+     *
+     * @param string $event
+     * @return array
+     */
+    public function broadcastOn($event)
+    {
+        if ($event === 'created') {
+            return [$this->station];
+        }
     }
 }
